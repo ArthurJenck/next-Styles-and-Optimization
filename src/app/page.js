73e8077d76b0f853845a200f18d17ca0ad73c14f
Arthur Jenck <arthurjenckdev@gmail.com>
2024-12-16 21:24:01 +0100

    feat: added sass

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import Card from "@/components/Card/Card"
+
 export default function Home() {
     return (
         <main>
             <h1 className="text-2xl font-mono">Styles et Optimisation</h1>
             <p className="font-sans">Arthur Jenck</p>
+            <Card />
         </main>
     )
 }
