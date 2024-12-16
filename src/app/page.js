2afc1bc8d59f99fc4739f3a17fecb4e6883638da
Arthur Jenck <arthurjenckdev@gmail.com>
2024-12-16 21:43:07 +0100

    feat: image optimization

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
 import Card from "@/components/Card/Card"
+import mountain from "../../public/images/mountain-1080.jpg"
+import Image from "next/image"
 
 export default function Home() {
     return (
-        <main>
+        <main className="max-w-5xl mx-auto">
             <h1 className="text-2xl font-mono">Styles et Optimisation</h1>
-            <p className="font-sans">Arthur Jenck</p>
+            <p className="font-sans mb-6">Arthur Jenck</p>
             <Card />
+
+            <Image
+                src={mountain}
+                alt="Montagne au lever du soleil"
+                sizes="
+                (max-width: 500px) 500px,
+                (max-width: 800px) 800px,
+                (max-width: 1080px) 1080px,
+                1080px
+                "
+            />
         </main>
     )
 }
